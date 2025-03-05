ed3b60ad2f1d6285ca237910e5f6c4889be7f4eb
Alex <7813306+mehov@users.noreply.github.com>
2025-03-05 15:43:37 +0100

    Add Http listener

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+import os
+import socketserver
 from src.Bot import Bot
+from src.Http import Http
+
 
 notifier = Bot()
 notifier.run()
+
+if os.getenv("HTTP_PORT") is not None:
+    HTTP_PORT = int(os.getenv("HTTP_PORT"))
+    with socketserver.TCPServer(("", HTTP_PORT), Http) as httpd:
+        print(f"Serving on port {HTTP_PORT}")
+        httpd.serve_forever()
---
+++ src/Http.py
@@ -0,0 +1,10 @@
+import http.server
+
+
+class Http(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello world!")
+
