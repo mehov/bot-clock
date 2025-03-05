373fb38b85a287cafd32a7dcf1b5faebc0a4f448
Alex <7813306+mehov@users.noreply.github.com>
2025-03-05 16:57:05 +0100

    Add ability to configure HTTP_HOST, default to 0.0.0.0

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ notifier.run()
 
 if os.getenv("HTTP_PORT") is not None:
     HTTP_PORT = int(os.getenv("HTTP_PORT"))
-    with socketserver.TCPServer(("", HTTP_PORT), Http) as httpd:
-        print(f"Serving on port {HTTP_PORT}")
+    HTTP_HOST = os.getenv("HTTP_HOST", "0.0.0.0")
+    with socketserver.TCPServer((HTTP_HOST, HTTP_PORT), Http) as httpd:
+        print(f"Serving on {HTTP_HOST}:{HTTP_PORT}")
         httpd.serve_forever()
