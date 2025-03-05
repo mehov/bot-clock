701a805dbbf6a831f0378350e72fc2d5ffb8d8d0
Alex <7813306+mehov@users.noreply.github.com>
2025-03-05 17:32:51 +0100

    Use the logging module

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ if os.getenv("HTTP_PORT") is not None:
     HTTP_PORT = int(os.getenv("HTTP_PORT"))
     HTTP_HOST = os.getenv("HTTP_HOST", "0.0.0.0")
     with socketserver.TCPServer((HTTP_HOST, HTTP_PORT), Http) as httpd:
-        print(f"Serving on {HTTP_HOST}:{HTTP_PORT}")
+        logging.info(f"Serving on {HTTP_HOST}:{HTTP_PORT}")
         httpd.serve_forever()
