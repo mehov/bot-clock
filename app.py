cc97f68e4339e4d568328fcfb4d042cbe94dcbfa
Alex <7813306+mehov@users.noreply.github.com>
2025-03-05 18:30:03 +0100

    Move Http to src/Http.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import socketserver
 import sys
 from src.Bot import Bot
 from src.Http import Http
@@ -12,12 +11,8 @@ for key, value in os.environ.items():
     logging.info(f"    {key} = {value}")
 
 # Start the HTTP listener
-HTTP_PORT = int(os.getenv("HTTP_PORT", "80"))
-HTTP_HOST = os.getenv("HTTP_HOST", "0.0.0.0")
-logging.info(f"Configured to serve on {HTTP_HOST}:{HTTP_PORT}")
-with socketserver.TCPServer((HTTP_HOST, HTTP_PORT), Http) as httpd:
-    logging.info(f"Serving on {HTTP_HOST}:{HTTP_PORT}")
-    httpd.serve_forever()
+http = Http()
+http.run()
 
 # Start the bot
 notifier = Bot()
--- src/Http.py
+++ src/Http.py
@@ -1,10 +1,24 @@
 import http.server
+import logging
+import os
+import socketserver
 
 
-class Http(http.server.SimpleHTTPRequestHandler):
+class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"Hello world!")
 
+
+class Http:
+    def run(self):
+        if os.getenv("HTTP_PORT") is None:
+            return
+        HTTP_PORT = int(os.getenv("HTTP_PORT"))
+        HTTP_HOST = os.getenv("HTTP_HOST", "0.0.0.0")
+        logging.info(f"Configured to serve on {HTTP_HOST}:{HTTP_PORT}")
+        with socketserver.TCPServer((HTTP_HOST, HTTP_PORT), Handler) as httpd:
+            logging.info(f"Serving on {HTTP_HOST}:{HTTP_PORT}")
+            httpd.serve_forever()
