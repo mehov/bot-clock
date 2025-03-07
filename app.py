b42a6a779a63e85b2415a0b85bf808cf425c9714
Alex <7813306+mehov@users.noreply.github.com>
2025-03-07 13:49:55 +0100

    Do not call app.run()

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
+import threading
 from src.Http import http as app
+from src.Bot import Bot
 # If Azure is running something like gunicorn -b 0.0.0.0:8000 app:app, then it expects an app variable in app.py.
 
 
-if __name__ == '__main__':
-    app.run()
+# app.run() # no need for this as Azure has a separate launcher
