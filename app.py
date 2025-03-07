5744c9484db19843a86cb5ce5e62af0a6069bba9
Alex <7813306+mehov@users.noreply.github.com>
2025-03-07 18:11:27 +0100

    Specify host and port for app.run()

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ if __name__ == '__main__':
     bot_thread = threading.Thread(target=bot.run)
     bot_thread.daemon = True  # This ensures the bot thread will stop when the main thread exits
     bot_thread.start()
-    app.run()
+    app.run(host=os.getenv("HTTP_HOST", "0.0.0.0"), port=int(os.getenv("HTTP_PORT")))
