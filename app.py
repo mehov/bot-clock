0c3d509934c42375097162e90a9b7f874b96a6dc
Alex <7813306+mehov@users.noreply.github.com>
2025-03-07 16:52:22 +0100

    Change how we use multiprocessing
    https://stackoverflow.com/questions/28800614/run-while-loop-concurrently-with-flask-server

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-import multiprocessing
+import asyncio
+import time
+from multiprocessing import Process, Value
 from src.Http import http as app
 from src.Bot import Bot
 
 # If Azure is running something like gunicorn -b 0.0.0.0:8000 app:app, then it expects an app variable in app.py.
 
 
-bot = Bot()
-
-def start_bot():
-    import asyncio
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(bot.run())
+def run_bot():
+    bot = Bot()
+    asyncio.run(bot.run())
 
 
-if __name__ == '__main__':
-    bot_process = multiprocessing.Process(target=start_bot, daemon=True)
-    bot_process.start()  # Start bot in a separate process
-    app.run()
+if __name__ == "__main__":
+    p = Process(target=run_bot)
+    p.start()
+    app.run(debug=True, use_reloader=False)
+    p.join()
