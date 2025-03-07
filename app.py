9329c75a8c4d4f5c72a0867e413184c74964e458
Alex <7813306+mehov@users.noreply.github.com>
2025-03-07 13:37:05 +0100

    Alias http as app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from src.Http import http
+from src.Http import http as app
+# If Azure is running something like gunicorn -b 0.0.0.0:8000 app:app, then it expects an app variable in app.py.
 
 
 if __name__ == '__main__':
-    http.run()
+    app.run()
