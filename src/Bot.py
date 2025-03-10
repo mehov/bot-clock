8fb272f01db375d4c68560a9eab7a3984ddccc80
Alex <7813306+mehov@users.noreply.github.com>
2025-03-10 19:05:00 +0100

    Try to reuse the same event loop

--- src/Bot.py
+++ src/Bot.py
@@ -8,10 +8,10 @@ class Bot:
         self.bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
         self.chat_id = os.getenv('TELEGRAM_CHAT_ID')
         self.telegram_bot = telegram.Bot(token=self.bot_token)
+        self.loop = asyncio.new_event_loop()
 
     def send_message(self, message):
         async def send_message_async():
             await self.telegram_bot.send_message(chat_id=self.chat_id, text=message)
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(send_message_async())
+        asyncio.set_event_loop(self.loop)
+        self.loop.run_until_complete(send_message_async())
