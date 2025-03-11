73df946a8bdf51f7653dde654c191b8e08ca9c8a
Alex <7813306+mehov@users.noreply.github.com>
2025-03-11 15:24:53 +0100

    Set webhook on bot start

--- src/Bot.py
+++ src/Bot.py
@@ -9,6 +9,14 @@ class Bot:
         self.chat_id = os.getenv('TELEGRAM_CHAT_ID')
         self.telegram_bot = telegram.Bot(token=self.bot_token)
         self.loop = asyncio.new_event_loop()
+        # set webhook if HTTP_HOSTNAME present in environment
+        hostname = os.getenv('HTTP_HOSTNAME')
+        if hostname is not None:
+            async def set_webhook():
+                webhook_url = f"https://{hostname}/webhook-endpoint"
+                await self.telegram_bot.set_webhook(webhook_url)
+            asyncio.set_event_loop(self.loop)
+            self.loop.run_until_complete(set_webhook())
 
     def send_message(self, message):
         async def send_message_async():
